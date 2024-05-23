4eaa99de7ceec0781e71aede2144806c965b3255
KilldenHI <killpochta@gmail.com>
2024-05-24 02:48:41 +0300

    Initial commit

---
+++ yandexdriver/src/main/java/ru/mirea/zherebtsov/yandexdriver/App.java
@@ -0,0 +1,15 @@
+package ru.mirea.zherebtsov.yandexdriver;
+
+import android.app.Application;
+
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "4e13f9cd-4f18-4e7f-837f-c92e7f5ab2ed";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Set the api key before calling initialize on MapKitFactory.
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
